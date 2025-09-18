5620a9caa9975db654d2e9c8ccd63573a8a4c5ff
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-09-18 13:49:59 +0200

    Car klasse plus test

---
+++ Bridge/Car.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Bridge
+{
+    /// <summary>
+    /// Car class with properties Licensplate and date and methods to get price and vehicle type.
+    /// </summary>
+    public class Car
+    {
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Gets the fixed price value.
+        /// </summary>
+        /// <returns>The price (230) as a double</returns>
+        public double Price()
+        {
+            return 230;
+        }
+
+        /// <summary>
+        /// Gets the type of the vehicle.
+        /// </summary>
+        /// <returns>A string representing the type of the vehicle. The default value is "Car".</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ BridgeTest/Test1.cs
@@ -0,0 +1,24 @@
+﻿using Bridge;
+using System.ComponentModel.DataAnnotations;
+
+namespace BridgeTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestPrice()
+        {
+            Car car = new Car();
+            var result = car.Price();
+            Assert.AreEqual(230, result);
+        }
+        [TestMethod]
+        public void TestVehicleType()
+        {
+            Car car = new Car();
+            var result = car.VehicleType();
+            Assert.AreEqual("Car", result);
+        }
+    }
+}
